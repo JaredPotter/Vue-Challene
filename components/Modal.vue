d5f9e6a6047c25c06c0399a6b6319257e83ec9f2
jpotter <jpotter@uhin.org>
2018-02-13 22:38:46 -0700

    Added jquery and bootstrap to npm. Also clean up.

--- components/Modal.vue
+++ components/Modal.vue
@@ -1,31 +1,12 @@
 <template>
 	<div>
 		
-
-
-
-
-	    <div id="box">
-	      <div class="boxSmall"></div>
-	      <div class="boxSmall boxTiny"></div>
-	    </div>
-
-
 	    <svg x="0px" y="0px" width="570px" height="150px" viewBox="0 0 570 150" xml:space="preserve">
 	      <circle class="circle-1" fill="none" cx="71.5" cy="77.5" r="51.5" stroke="#88CE02" stroke-width="4"/>
 	    </svg>
 
-
 	    <button v-on:click="anim_svg">Animate</button>
 
-
-
-
-
-
-
-		<!-- Button trigger modal -->
-		<!-- <button id="modal-button" type="button"> -->
 		<div v-if="!displayed">
 			<svg class="modal-button" viewBox="0 0 300 100" @click="showModal()">
 				<text x="45" y="65" font-family="Verdana" font-size="36">{{ buttonText }}</text>
@@ -41,12 +22,6 @@
 			 	<text x="120" y="85" fill="white" font-family="Verdana" font-size="12" cursor="pointer" text-anchor="middle" @click="closeModal()">{{modalButtonText}}</text>
 			</svg>
 		</div>
-		<div>
-			<svg>
-				<rect id="myRect" fill="none" stroke-width="6" stroke="purple" width="100" height="100"/>
-				<path id="path" d="M10 80 C 40 10, 65 10, 95 80 S 150 150, 180 80" stroke="black" stroke-width="5" fill="transparent"/>
-			</svg>
-		</div>
 	</div>
 </template>
 
@@ -54,25 +29,6 @@
 
 	import TweenMax from 'gsap'
 	import DrawSVGPlugin from "gsap/DrawSVGPlugin"
-	// import { TweenLite , drawSVGPlugin } from 'gsap';
-
-	// import { TweenLite , drawSVGPlugin } from 'gsap/plugins/DrawSVGPlugin';
-	// import TweenLite from 'gsap/TweenLite';
-	//import { TweenLite, DrawSVGPlugin } from "gsap"
-	// import DrawSVGPlugin from 'gsap/DrawSVGPlugin';
-// var DrawSVGPlugin = require("gsap/src/minified/plugins/DrawSVGPlugin");
-	// import DrawSVGPlugin from 'gsap/src/minified/plugins/DrawSVGPlugin.min.js';
-	// import 'gsap/DrawSVGPlugin';
-	// import DrawSVGPlugin from 'gsap';
-	// var DrawSVGPlugin = require("gsap/plugins/drawsvgplugin"); 
-	// import DrawSVGPlugin from 'gsap/plugins/drawSVGPlugin'; 
-	// import { drawSVG } from 'gsap/plugins/DrawSVGPlugin';
-	// import { drawSVGPlugin } from 'gsap/plugins';
-
-	// nope
-	// import '../node_modules/gsap/plugins/DrawSVGPlugin.js';
-	// import 'gsap/DrawSVGPlugin';
-	// import { DrawSVGPlugin } from 'greensock-js-business-green';
 
 	export default {
 		data() {
@@ -85,23 +41,11 @@
 			};
 		},
 		mounted (){
-			// TweenMax.set('#path', {drawSVG: "40% 60%"})
-			
 
-			//TweenMax.to('#path', 2, {drawSVG: "40% 60%", transformOrigin:"50% 50%", rotation: 360});
-
-
-			// TweenLite.to('#path', 2, {drawSVG: true});
-			//var d = new DrawSVGPlugin();
-			// TweenLite.set('#path', {drawSVG: "40% 60%"});
 		},
 		methods: {
 			anim_svg() {
-				 
-
-				var box = document.getElementById("box");
-				TweenLite.to(box, 0.7, {left: 0, x: 0});
-
+				
 			    TweenLite.fromTo(".circle-1", 1, {drawSVG:"0 5%"}, {drawSVG:"9% 100%"});
 			},
 			showModal() {
@@ -112,28 +56,8 @@
 					darkDiv.id = 'dark-div';
 					document.body.appendChild(darkDiv);
 
-					// var tl = new TimelineLite();
-					// TweenLite.set('#modal-rect', {stroke: "red", strokeWidth:5});
-
-
-					// tl.to('.modal-button', 2.5, {rotation: 360, scale: 5, transformOrigin:"50% 50%", y: -420});
-						// .staggerFromTo('rect', 1, {drawSVG:"100%"}, {drawSVG:"50% 50%"}, 0.1)
-
-					// .from('rect', 2.5, {drawSVG: 0});
-
-					// tl.timeScale(2);
-
-					// TweenLite.set("#myRect", {drawSVG: 0});
-					TweenLite.set("#myRect", {visibility:"invisable"});
-
-
-					TweenLite.set('#path', {drawSVG: "40% 60%"})
-
-					// var tl2 = new TimelineMax();
-					// tl2.from('rect', 2.5, {drawSVG: true});
-					// TweenLite.from('rect', 2.5, {drawSVG: "50% 50%"});
-					// TweenLite.to('.modal-button', 0.5, {rotation: 360, scale: 5, transformOrigin:"50% 50%", y: -420, drawSVG: 0});
-
+					var tl = new TimelineLite();
+					tl.to('.modal-button', 0.5, {rotation: 360, scale: 5, transformOrigin:"50% 50%", y: -420});
 
 					this.displayed = true;
 				}
@@ -142,18 +66,10 @@
 				if(this.displayed == true) {
 					document.body.className = '';
 					document.getElementById("dark-div").remove();
+
 					var tl = new TimelineLite();
-					// TweenLite.set('#modal-rect', {stroke: "red", strokeWidth:5});
+					tl.to('.modal-content', 0.5, {rotation: -360, scale: 1, transformOrigin:"50% 50%", y: 0});
 
-
-					tl.to('.modal-content', 2.5, {rotation: -360, scale: 1, transformOrigin:"50% 50%", y: 0})
-					.to('rect', 2.5, {drawSVG: "100%"}, 1.5);
-
-					// tl.timeScale(2);
-
-					var tl2 = new TimelineLite();
-					tl2.from('#myRect', 2.5, {drawSVG: "100%"}, {drawSVG: "50% 50%"}, 1.5);
-					// TweenLite.to('.modal-content', 0.5, {rotation: -360, scale: 1, transformOrigin:"50% 50%", y: 0, drawSVG: 0});
 					this.displayed = false;
 				}
 			}
@@ -177,10 +93,9 @@
     stroke-width: 1px;
 }
 
-/*#modal-rect, text {
-	strokeWidth: 1px;
-	stroke: red;
-}*/
+#dark-div {
+	opacity: 0.7;
+}
 
 .display {
 	display: block;
@@ -201,13 +116,6 @@
     color: #3b8070;
     stroke-width: 1px;
 }
-
-
-
-
-
-
-
 
 #box {
   background-color: #88ce02;
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,14 +1,30 @@
+// Pre-render code.
 // var path = require('path');
 // var PrerenderSpaPlugin = require('prerender-spa-plugin');
-// var config = require('./nuxt.config.js');
-//require("gsap");
+
+var webpack = require('webpack');
+
 module.exports = {
-  // mode: 'spa',
-
-  // plugins: [
-  // '~/plugins/vue-js-modal'
-  // ],
-
+  build: {
+    vendor: ['jquery', 'bootstrap'],
+      plugins: [
+      // set shortcuts as global for bootstrap
+      new webpack.ProvidePlugin({
+        $: 'jquery',
+        jQuery: 'jquery',
+        'window.jQuery': 'jquery'
+      })
+    ]
+  },
+  css: ['bootstrap/dist/css/bootstrap.css'],
+  // include bootstrap js on startup
+  plugins: [
+    '~plugins/bootstrap.js',
+    // new PrerenderSpaPlugin(
+    //     path.join(__dirname, '../dist'),
+    //     [ '/', '/about' ]
+    //   )
+  ],
   head: {
     title: 'vue-challenge',
     meta: [
@@ -17,63 +33,25 @@ module.exports = {
       { hid: 'description', name: 'description', content: 'Nuxt.js project' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'stylesheet', href: 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css'},
-      { rel: 'stylesheet', href: 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css'}
-    ],
-    script: [
-      {type: 'text/javascript', src:"https://code.jquery.com/jquery-3.3.1.min.js"},
-      {type: 'text/javascript', src:"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"},
-      {type: 'text/javascript', src:"https://cdnjs.cloudflare.com/ajax/libs/gsap/1.18.2/plugins/CSSPlugin.min.js"},
-      {type: 'text/javascript', src:"https://cdnjs.cloudflare.com/ajax/libs/gsap/1.20.3/TweenLite.min.js"}
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
   /*
   ** Customize the progress bar color
   */
-  loading: { color: '#3B8070' },
+  loading: { color: '#3B8070' }
+
   /*
-  ** Build configuration
+  ** Run ESLint on save
   */
-  // build: {
-
-  //     // extend (config, {isClient, dev}) {
-  //     //   if (isClient) {
-
-  //     //     config.entry['app'] = '~/node_modules/gsap/src/uncompressed/plugins/DrawSVGPlugin';
-  //     //   }
-
-  //   },
-    //plugins: ["~plugins/DrawSVGPlugin.js"],
-    // extend(config, {isDev, isClient}) {
-    //   if (isClient) {
-    //     config.entry.app = ["~/plugins/DrawSVGPlugin.min.js", config.entry.app];
-    //   }
-    // },
-    // extend (config, ctx) {
-    //   if (ctx.isClient) {
-    //     config.entry.app = ['gsap','~plugins/DrawSVGPlugin', config.entry.app];
-    //   }
-    // }
-    /*
-    ** Run ESLint on save
-    */
-//     plugins: [
-//       new PrerenderSpaPlugin(
-//         path.join(__dirname, '../dist'),
-//         [ '/', '/about' ]
-//       )
-// 'vue-js-modal'
-//     ],
-    // extend (config, { isDev, isClient }) {
-    //   if (isDev && isClient) {
-    //     config.module.rules.push({
-    //       enforce: 'pre',
-    //       test: /\.(js|vue)$/,
-    //       loader: 'eslint-loader',
-    //       exclude: /(node_modules)/
-    //     })
-    //   }
-    // }
-  
+  // extend (config, { isDev, isClient }) {
+  //   if (isDev && isClient) {
+  //     config.module.rules.push({
+  //       enforce: 'pre',
+  //       test: /\.(js|vue)$/,
+  //       loader: 'eslint-loader',
+  //       exclude: /(node_modules)/
+  //     })
+  //   }
+  // }
 };
